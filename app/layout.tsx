288afeadb22431a458c19c5fc4e09121e34bc4dd
Nandini yadav <nandaniyadav521@gmail.com>
2026-01-09 15:50:14 +0530

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 // app/layout.tsx
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/footer";
 import "./globals.css";
 import type { Metadata } from "next";
-import Footer from "@/components/footer";
-
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Double Trouble Studio",
@@ -17,14 +17,43 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-dts-black text-dts-fog" 
-          suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5J6M99HD');
+            `,
+          }}
+        />
+      </head>
+
+      <body
+        className="bg-dts-black text-dts-fog"
+        suppressHydrationWarning
+      >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5J6M99HD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Navbar />
-       
+
         {/* IMPORTANT: no max-w here */}
         <main className="min-h-screen">
           {children}
-          <Footer/>
+          <Footer />
         </main>
       </body>
     </html>
