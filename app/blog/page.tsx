df6b2621517b97e76895816851338de93ce39468
Nandini yadav <nandaniyadav521@gmail.com>
2026-04-04 13:11:50 +0530

    updated blog page schema

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -41,6 +41,66 @@ export default function BlogPage() {
         }}
       />
 
+      <Script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify([
+      {
+        "@context": "https://schema.org",
+        "@type": "Blog",
+        "@id": "https://www.dtsworld.in/blog#blog",
+        "name": "Double Trouble Studio Blog",
+        "description":
+          "Insights, strategies and real case studies from PR, celebrity, events and digital marketing projects.",
+        "url": "https://www.dtsworld.in/blog",
+        "publisher": {
+          "@type": "Organization",
+          "name": "Double Trouble Studio",
+          "logo": {
+            "@type": "ImageObject",
+            "url": "https://www.dtsworld.in/logo.png"
+          }
+        }
+      },
+
+      {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "@id": "https://www.dtsworld.in/#organization",
+        "name": "Double Trouble Studio",
+        "url": "https://www.dtsworld.in",
+        "logo": {
+          "@type": "ImageObject",
+          "url": "https://www.dtsworld.in/logo.png"
+        },
+        "sameAs": [
+          "https://www.instagram.com/",
+          "https://www.linkedin.com/"
+        ]
+      },
+
+      {
+        "@context": "https://schema.org",
+        "@type": "BreadcrumbList",
+        "itemListElement": [
+          {
+            "@type": "ListItem",
+            "position": 1,
+            "name": "Home",
+            "item": "https://www.dtsworld.in"
+          },
+          {
+            "@type": "ListItem",
+            "position": 2,
+            "name": "Blog",
+            "item": "https://www.dtsworld.in/blog"
+          }
+        ]
+      }
+    ]),
+  }}
+/>
+
       {/* ✅ ONLY THIS */}
       <BlogClient posts={posts} />
     </>
